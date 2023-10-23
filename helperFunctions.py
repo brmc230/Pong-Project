e953dd329580973d4cce429b82f55540971b69ac
Brooke McWilliams <brmc230@gmail.com>
2023-10-23 13:01:27 -0400

    Implemented an update server function from the client

--- helperFunctions.py
+++ helperFunctions.py
@@ -17,10 +17,20 @@
 #               changed global variable X to Y>
 
 # ================================================================================================== #
+import socket
+from assets.code.helperCode import *
 
 # Author:       Brooke McWilliams
-# Purpose:      
-# Pre:          <What preconditions does this method expect to be true? Ex. This method 
-#               expects the program to be in X state before being called> 
-# Post:         <What postconditions are true after this method is called? Ex. This method 
-#               changed global variable X to Y>
+# Purpose:      Client updates the server on paddle information, ball location, and the
+#               current score of the game
+# Pre:          When this method is called the game is expected to be finished updating the
+#               clients individual information 
+# Post:         This method sends to the server the clients information
+
+def update_server(paddleInfo:Paddle, ballInfo:Ball, scoreL:int, scoreR:int, client:socket.socket) -> None:
+    try: 
+        client_information = f"playerPaddle: {paddleInfo}, ball: {ballInfo}, lScore: {scoreL}, rScore: {scoreR}"
+        client.send(client_information.encode())
+
+    except Exception as e:
+        print(f"Error: {e}")
