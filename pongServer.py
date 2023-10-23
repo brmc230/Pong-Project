6929565553baafc55c2279f509cff5a01978e35f
Brooke McWilliams <brmc230@gmail.com>
2023-10-23 13:00:48 -0400

    Simple server basis added

--- pongServer.py
+++ pongServer.py
@@ -1,16 +1,12 @@
 # =================================================================================================
 # Contributing Authors:	    Brooke McWilliams
 #                           
-#
 # Email Addresses:          brmc230@uky.edu
-#
-#                           
+#                        
 # Date:                     10/23/2023
-#
 # Purpose:                  Code that implements the server side of the Pong game and interacts with
 #                           the client
-#
-# Misc:                     <Not Required.  Anything else you might want to include>
+# Misc:                     
 # =================================================================================================
 
 import socket
@@ -22,6 +18,19 @@ import threading
 #   for each player and where the ball is, and relay that to each client
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 #   clients are and take actions to resync the games
-
 # Make precautions for if someone enters the wrong IP or port and if a 3rd person attempts to 
-# connect to an already full game
+#   connect to an already full game
+
+# Dont know yet about getting this info, just rolling with it
+host = ""
+ip = ""
+
+# Create the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # For local host use
+server.bind((host, ip))
+
+# Getting client information
+client_socket, client_ip = server.accept()
+
+
