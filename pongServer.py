20f0f6daf94d485c6bd93991fe19e2fb8b194c00
Brooke McWilliams <brmc230@gmail.com>
2023-10-25 14:14:14 -0400

    Implemented more server logic

--- pongServer.py
+++ pongServer.py
@@ -28,12 +28,11 @@ import pyautogui
 # Pre:          This function carries out the instructions for the client
 # Post:         Sends the client the information it was asking for
 
-def handle_client(client_ID:str) -> None: 
-    client = client_sockets_dict[client_ID]
+def handle_client(client_soc:socket.socket, client:str) -> None: 
     connected = True
     try:
         while connected: 
-            response = client.recv(1024).decode()
+            response = client_soc.recv(1024).decode()
             # Client disconnected
             if not response:
                 connected = False
@@ -46,18 +45,19 @@ def handle_client(client_ID:str) -> None:
 
                 # Update game state conditions here and call appropriate functions
                 if client_request == "join_server":
-                    join_server_response(client_ID)
+                    join_server_response(client_soc)
                 elif client_request == "client_game_data":
-                    update_client_data(client_ID, client_request)
+                    update_client_data(client, client_request)
+                elif client_request == "server_update":
+                    server_update_response(client_sockets_dict, client, client_soc)
 
 
 
 
-
-                # Update the other client 
-                for clients in client_sockets_dict:
-                    if clients != client_ID:
-                        client_sockets_dict[clients].send(json.dumps(game_state).encode())
+                # Update other clients values
+                for key, value in client_sockets_dict.items():
+                    if value != client_soc:
+                        client_sockets_dict[key].send(json.dumps(game_state).encode())
 
             except Exception as e:
                 print(f"Error processing client request: {str(e)}")
@@ -74,13 +74,10 @@ def handle_client(client_ID:str) -> None:
 # Pre:          Client runs the joinServer function
 # Post:         Server sends corresponding information back to client
 
-def join_server_response(client:str) -> None:
-    client_soc = client_sockets_dict[client]
-    
-    join_server_response_data = {   "screen_width": 1920,
-                                    "screen_height": 1080,
+def join_server_response(client_soc:socket.socket) -> None:
+    join_server_response_data = {   "screen_width": 1600,
+                                    "screen_height": 900,
                                     "player_paddle": game_state[client_ID]["paddle"] }
-# Don't know what to send here because I don't yet know how to receieve it or what im actually sending?
     client_soc.send(json.dumps(join_server_response_data).encode())
 
 # ======================================================================================================================= #
@@ -90,19 +87,49 @@ def join_server_response(client:str) -> None:
 # Pre:          Client sends an update of their data
 # Post:         Server updates corresponding values for the client
 
-def update_client_data(client_ID:str, data:dict) -> None:
-    for key in data:
+def update_client_data(client_id:str, data:dict) -> None:
+    for key, value in data.items():
         if key == "playerPaddle":
-            game_state[client_ID]["paddle_loc"] = data["playerPaddle"]
+            game_state[client_id]["paddle_loc"] = value
+        elif key == "opPaddle":
+            game_state[client_id]["paddle_locOP"] = value
         elif key == "ball":
-            game_state[client_ID]["ball_loc"] = data["ball"]
+            game_state[client_id]["ball_loc"] = value
         elif key == "lScore":
-            game_state[client_ID]["lScore"] = data["lScore"]
+            game_state[client_id]["lScore"] = value
         elif key == "rScore":
-            game_state[client_ID]["rScore"] = data["rScore"]
+            game_state[client_id]["rScore"] = value
         elif key == "sync":
-            game_state[client_ID]["sync"] = data["sync"]
+            game_state[client_id]["sync"] = value
         
+# ======================================================================================================================= #
+
+# Author:       Brooke McWilliams
+# Purpose:      Handle updating the client from the server
+# Pre:          Client request to sync with other client
+# Post:         Server sends update and fixes sync issues
+
+def server_update_response(clients:dict, clientid:str, clientSoc:socket.socket) -> None:
+    for key in clients:
+        if key != clientid:
+            opponent_subdict = clients[key]
+            for subkey, value in opponent_subdict.items():
+                if subkey == "sync":
+                    if value > game_state[clientid][subkey]:
+                        game_state[clientid]["paddle_locOP"] = opponent_subdict["paddle_loc"]
+                        game_state[clientid]["ball"] = opponent_subdict["ball"]
+                        game_state[clientid]["lScore"] = opponent_subdict["lScore"]
+                        game_state[clientid]["rScore"] = opponent_subdict["rScore"]
+                        game_state[clientid]["sync"] = opponent_subdict["sync"]
+                    elif value < game_state[clientid][subkey]:
+                        opponent_subdict["paddle_locOP"] = game_state[clientid]["paddle_loc"]
+                        opponent_subdict["ball"] = game_state[clientid]["ball"]
+                        opponent_subdict["lScore"] = game_state[clientid]["lScore"]
+                        opponent_subdict["rScore"] = game_state[clientid]["rScore"]
+                        opponent_subdict["sync"] = game_state[clientid]["sync"]
+            
+    clientSoc.send(json.dumps(game_state[clientid]).encode())
+
 # ======================================================================================================================= #
 
 # Initialize the server specs, don't know if this is 100% correct yet
@@ -115,7 +142,6 @@ server_socket.bind((server_host, server_ip))
 
 # Listen for clients
 server_socket.listen(5)
-
 
 semaphore = threading.Semaphore()
 client_sockets_dict = dict()
@@ -136,6 +162,7 @@ while connected_players < 2:
     if not first_client:
         game_state[client_ID]["paddle"] = "right"
         game_state[client_ID]["paddle_loc"] = ""
+        game_state[client_ID]["paddle_locOP"] = ""
         game_state[client_ID]["ball_loc"]= ""
         game_state[client_ID]["rScore"] = 0
         game_state[client_ID]["lScore"] = 0
@@ -144,6 +171,7 @@ while connected_players < 2:
     else:
         game_state[client_ID]["paddle"] = "left"
         game_state[client_ID]["paddle_loc"] = ""
+        game_state[client_ID]["paddle_locOP"] = ""
         game_state[client_ID]["ball_loc"]= ""
         game_state[client_ID]["rScore"] = 0
         game_state[client_ID]["lScore"] = 0
@@ -152,6 +180,13 @@ while connected_players < 2:
 
 while game_over is False:
     # Handle the clients with threads
-    handle_client = threading.Thread(target=handle_client, args=(client_ID,))
-    handle_client.start()
+    for key, value in client_sockets_dict.items():
+        handle_client = threading.Thread(target=handle_client, args=(value, key ))
+        handle_client.start()
+
+for value in client_sockets_dict.values():
+    value.close()
+
+server_socket.close()
+
 
